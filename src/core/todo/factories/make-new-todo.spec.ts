1a2731e75ccbd4fe8346ba094b4593768fe048f7
Rodrigo Silva <rdg.hora1@gmail.com>
2025-08-01 09:17:24 -0300

    primeiro teste unitário de uma função (Testes automatizados)

---
+++ src/core/todo/factories/make-new-todo.spec.ts
@@ -0,0 +1,28 @@
+import { makeNewTodo } from "./make-new-todo";
+describe("makeNewTodo (unit)", () => {
+  describe("create", () => {
+    test("deve retornar um novo todo válido", () => {
+      // AAA -> Arrange, Act, Assert
+
+      // Arrange: prepara os dados necessários para o teste
+      const expectedTodo = {
+        id: expect.any(String), // O ID é gerado aleatoriamente, então usamos expect.any para aceitar qualquer string
+        description: "meu novo todo",
+        createdAt: expect.any(String), // A data de criação é gerada automaticamente, então usamos expect.any para aceitar qualquer string
+      };
+
+      // Act: chama a função que está sendo testada
+      const newTodo = makeNewTodo("meu novo todo");
+
+      // Assert: verifica se o resultado está correto
+      // toBe é o === para tipos primitivos (string, number, etc.)
+      // toEqual ou toStrictEqual é para objetos
+
+      // Checando apenas a descrição
+      expect(newTodo.description).toBe(expectedTodo.description);
+
+      // Checando o objeto inteiro
+      expect(newTodo).toStrictEqual(expectedTodo);
+    });
+  });
+});
