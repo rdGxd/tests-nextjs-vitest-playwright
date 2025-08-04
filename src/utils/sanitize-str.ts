392c48aa570968e978f17edb86563bb4e62de873
Rodrigo Silva <rdg.hora1@gmail.com>
2025-08-04 08:52:09 -0300

    Criando o schema do TODO para o Drizzle ORM (Tabela na base de dados)

--- src/utils/sanitize-str.ts
+++ src/utils/sanitize-str.ts
@@ -1,9 +1,7 @@
-import { getFullEnv } from "@/env/configs";
-
 export function sanitizeStr(str: string): string {
   const clean = !str || typeof str !== "string" ? "" : str.trim().normalize();
   return clean;
 }
 
-const env = getFullEnv();
-console.log(env);
+// const env = getFullEnv();
+// console.log(env);
