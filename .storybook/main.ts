fdf9f67b7c0c3da571a1d14d4d6249cc9accf63e
Rodrigo Silva <rdg.hora1@gmail.com>
2025-08-05 10:17:41 -0300

    Subindo o storybook e criando outros stories com os scripts do package.json

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,14 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: [
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
     '../src/**/stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-essentials'],
-  framework: '@storybook/nextjs',
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
   staticDirs: ['../public'],
   features: {
     backgroundsStoryGlobals: false,
--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,14 +3,46 @@ import { Button } from '.';
 
 const meta = {
   component: Button,
+  decorators: [
+    Story => (
+      <div className='max-w-screen mx-auto p-12 flex items-center justify-center'>
+        <Story />
+      </div>
+    ),
+  ],
 } satisfies Meta<typeof Button>;
 
 export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const Playground: Story = {
+export const Default: Story = {
   args: {
     children: 'Texto do botão',
+    variant: 'default',
+    size: 'lg',
+  },
+  // render: args => {
+  //   return (
+  //     <div className='max-w-screen mx-auto p-12 flex items-center justify-center bg-red-300'>
+  //       <Button {...args} />
+  //     </div>
+  //   );
+  // },
+};
+
+export const Danger: Story = {
+  args: {
+    children: 'Texto do botão',
+    variant: 'danger',
+    size: 'lg',
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    children: 'Texto do botão',
+    variant: 'ghost',
+    size: 'lg',
   },
 };
