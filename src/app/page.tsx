5df63ac1b45da402f3760c71aeedee111559d47f
Rodrigo Silva <rdg.hora1@gmail.com>
2025-08-07 09:41:49 -0300

    getByRole, beforeEach e afterAll no Playwright com Next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-import { TodoContainer } from '@/components/TodoContainer';
+import { TodoContainer } from "@/components/TodoContainer";
+import { Metadata } from "next";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Lista de Tarefas",
+};
 
 export default function Home() {
   return <TodoContainer />;
--- tests/home.e2e.ts
+++ tests/home.e2e.ts
@@ -1,12 +1,49 @@
-import { test } from "@playwright/test";
+import { makeTestTodoRepository } from "@/core/__tests__/utils/make-test-todo-repository";
+import { expect, Page, test } from "@playwright/test";
+
+const HOME_URL = "/";
+const HEADING = "Lista de tarefas";
+const INPUT = "Tarefa";
+const BUTTON = "Adicionar";
+const BUTTON_BUSY = "Adicionando...";
+const NEW_TODO_TEXT = "Nova tarefa de teste";
+
+const getHeading = (p: Page) => p.getByRole("heading", { name: HEADING });
+const getInput = (p: Page) => p.getByRole("textbox", { name: INPUT });
+const getButton = (p: Page) => p.getByRole("button", { name: BUTTON });
+const getButtonBusy = (p: Page) => p.getByRole("button", { name: BUTTON_BUSY });
+
+const getAll = (p: Page) => ({
+  heading: getHeading(p),
+  input: getInput(p),
+  button: getButton(p),
+  buttonBusy: getButtonBusy(p),
+});
+
+test.beforeEach(async ({ page }) => {
+  const { deleteTodoNoWhere } = await makeTestTodoRepository();
+  await deleteTodoNoWhere();
+  await page.goto(HOME_URL);
+});
+
+test.afterAll(async () => {
+  const { deleteTodoNoWhere } = await makeTestTodoRepository();
+  await deleteTodoNoWhere();
+});
 
 test.describe("Home Page E2E Tests", () => {
   // Renderização
   test.describe("Renderização", () => {
     test("deve ter o title correto", async ({ page }) => {
-      await page.goto("/");
-      const title = await page.title();
-      // test.expect(title).toBe("Todo App");
+      await expect(page).toHaveTitle("Lista de Tarefas");
+    });
+
+    test("deve renderizar o cabeçalho, o input e o botão para criar TODOs", async ({
+      page,
+    }) => {
+      await expect(getHeading(page)).toBeVisible();
+      await expect(getInput(page)).toBeVisible();
+      await expect(getButton(page)).toBeVisible();
     });
   });
   // Criação
